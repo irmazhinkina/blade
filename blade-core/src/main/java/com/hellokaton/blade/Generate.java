e2c3f5c531069cfa1b751870b4a87e881e95107e
irmazhinkina <irma.zhinkina@jetbrains.com>
2023-09-12 14:26:47 +0000

    111
    Signed-off-by: irmazhinkina <irma.zhinkina@jetbrains.com>

---
+++ blade-core/src/main/java/com/hellokaton/blade/Generate.java
@@ -0,0 +1,12 @@
+public class Generate {
+    int xxx = 5;
+
+    public Generate(int xxx) {
+        this.xxx = xxx;
+    }
+
+    public static void main(String[] args) {
+        Generate myObj = new Generate();
+        System.out.println(myObj.xxx);
+    }
+}
