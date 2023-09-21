f3ad6612a208ac5e2bbbbc6e2aa03737bee46c75
irmazhinkina <irma.zhinkina@jetbrains.com>
2023-09-21 13:17:47 +0000

    rename xxx
    Signed-off-by: irmazhinkina <irma.zhinkina@jetbrains.com>

---
+++ blade-core/src/main/java/com/hellokaton/blade/Generate.java
@@ -0,0 +1,13 @@
+public class Generate {
+    int yyy = 5;
+    yyy = 7;
+    yyy = 8;
+    public Generate() {
+        this.yyy = yyy;
+    }
+
+    public static void main(String[] args) {
+        Generate myObj = new Generate();
+        System.out.println(myObj.yyy);
+    }
+}
